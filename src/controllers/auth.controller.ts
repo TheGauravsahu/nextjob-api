0ec3250f8800ebae8822ef6ed846cf604aa63ff1
TheGauravsahu <itsgauravsahu7@gmail.com>
2025-05-10 10:32:27 +0530

    feat: add path to cookies

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -83,8 +83,9 @@ export const loginUser = async (
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "none",
+      path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
